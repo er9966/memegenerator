28c7f863bf364c4ce2d8703103eab9919e73e7e4
Chongjiu Gao <c.gao@f5.com>
2022-08-12 11:10:01 -0700

    i've got the memes

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [templates, setTemplates] = useState([]);
+
+  // fetch all the memes at once
+  useEffect(() => {
+    // fetching from API, read as json
+    fetch('https://api.imgflip.com/get_memes').then(x => 
+    // keep the memes as an array state
+      x.json().then(response => setTemplates(response.data.memes)))
+  } , [])
+
+
+
+  return (<div style={{textAlign: "center"}}>{
+    templates.map(template => {
+      return <img key = {template.id} src ={template.url} alt={template.name}/>;
+
+    })
+
+    }
+    
+
+  </div>);
 }
 
 export default App;
